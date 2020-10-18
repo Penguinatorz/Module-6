8082bc5da66c177868d5a5d86a25d9acd4726f52
Penguinatorz <70828847+Penguinatorz@users.noreply.github.com>
2020-10-18 06:45:49 -0400

    Add files via upload

---
+++ src/M6Test.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+
+/*
+ * Jancarlo Sevilla
+ * 10/17/2020
+ * Module-6-UI Design
+ * 
+ */
+
+
+
+public class M6Test { 
+
+	public static void main(String args[]) 
+	{ 
+		
+		Application.launch(Filechooser.class, args);
+
+	} 
+}
